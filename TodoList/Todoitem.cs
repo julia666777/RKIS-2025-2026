48d35fb113d5e2a3c9107afe3f1f0fc8660106ce
julia66677 <a.a.ch8535@gmail.com>
2025-10-22 19:19:31 +0300

    Lesson 6: Create TodoItem task 2

---
+++ TodoList/Todoitem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TodoList;
+internal class Todoitem
+{
+	public string Text { get; set; }
+	public bool IsDone { get; set; }
+	public DateTime LastUpdate { get; set; }
+
+	public Todoitem(string text)
+	{
+		Text = text;
+		IsDone = false;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void MarkDone()
+	{
+		IsDone = true;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void UpdateText(string newText)
+	{
+		Text = newText;
+		LastUpdate = DateTime.Now;
+	}
+
+	public string GetShortInfo()
+	{
+		string status = IsDone ? "Done" : "Pending";
+		string truncatedText = Text.Length > 30 ? Text.Substring(0, 30) + "..." : Text;
+		return $"{truncatedText} ({status}, {LastUpdate.ToShortDateString()})";
+	}
+
+	public string GetFullInfo()
+	{
+		string status = IsDone ? "Выполнено" : "В процессе";
+		return $"Задача: {Text}\nСтатус: {status}\nПоследнее обновление: {LastUpdate}";
+	}
+}
